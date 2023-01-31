1f285d29e58c92808dd6a1ef85fb1dfe54708a04
bachvq <baveku@gmail.com>
2023-01-31 10:24:57 +0700

    fix build

--- Package.swift
+++ Package.swift
@@ -21,7 +21,6 @@ let package = Package(
         .package(name: "SwiftProtobuf", url: "https://github.com/apple/swift-protobuf.git", .upToNextMajor(from: "1.18.0")),
         .package(name: "Promises", url: "https://github.com/google/promises.git", .upToNextMajor(from: "2.0.0")),
         .package(url: "https://github.com/apple/swift-log.git", .upToNextMajor(from: "1.4.2")),
-        .package(url: "https://github.com/apple/swift-collections.git", .upToNextMajor(from: "1.0.1")),
         .package(url: "https://github.com/daltoniam/Starscream", .upToNextMajor(from: "3.1.1"))
 ],
     targets: [
